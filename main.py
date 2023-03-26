47c69528581dbd982dc6d86cbde1450716d35826
sknoricklong <seankeenanlong@gmail.com>
2023-03-26 16:35:21 -0400

    debug

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ import pandas as pd
 import time
 from casescraper import CaseScraper
 
-@st.cache_resource
 def load_dataframes():
     alias_df = pd.read_csv("data/alias.csv")
     sentence_df = pd.read_csv("data/sentence.csv", dtype={'id': str, 'prison_sentence': float})
     profile_df = pd.read_csv("data/profile.csv", dtype={'id': str})
     return alias_df, sentence_df, profile_df
 
-@st.cache_resource
 def filter_alias_df(alias_df, first_name, last_name):
     first_name, last_name = first_name.lower(), last_name.lower()
     filtered_df = alias_df[alias_df['first_name'].str.lower().eq(first_name) & alias_df['last_name'].str.lower().eq(last_name)]
     return filtered_df.astype(str)
 
-@st.cache_resource
 def filter_sentence_df(sentence_df, id):
     filtered_df = sentence_df.loc[sentence_df['id'].eq(id)].astype(str).copy()
     filtered_df.loc[:, 'crf_number'] = filtered_df['crf_number'].apply(modify_crf_number)
     filtered_df.loc[:, 'community_sentence'] = filtered_df['community_sentence'].astype(float)
     return filtered_df.reset_index(drop=True)
 
-@st.cache_resource
 def search_profile(profile_df, id):
     official_last_name = None
     official_first_name = None
@@ -49,7 +45,6 @@ def modify_crf_number(value):
         num_parts[0] = '19' + num_parts[0] if num >= 24 else '20' + num_parts[0]
     return '-'.join(num_parts)
 
-@st.cache_resource
 def scrape_fee_table(county, case_number, first_name, last_name, middle_name=''):
     scraper = CaseScraper(county, case_number, first_name, last_name, middle_name)
     return scraper.fee_table, scraper.fee_table_issued, scraper.url
